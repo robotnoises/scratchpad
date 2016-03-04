4a2e11844a7bdbae761a572dffd2133d76906705
nicholsd <nicholsd@opentext.com>
2016-03-04 10:29:20 -0500

    Least common multiple

---
+++ least-common_multiple.js
@@ -0,0 +1,30 @@
+function lcm(n1, n2) {
+  
+  var multiple;
+  var multiplier = 1;
+  var notFound = true;
+  
+  function isMultiple(multiple, number) {
+    return (multiple % number) === 0;
+  }
+  
+  if (typeof n1 === 'number' && typeof n2 === 'number') {
+    
+    while (notFound) {
+      
+      multiple = n1 * multiplier;
+      
+      if (isMultiple(multiple, n2)) {
+        notFound = false;
+      } else {
+        multiplier++;
+      }
+    }
+    return multiple
+  }
+}
+
+var leastCommonMultiple = lcm(44, 7);
+
+console.log(leastCommonMultiple);
+
